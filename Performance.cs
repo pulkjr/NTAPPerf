37439d8bd572fc8a13c8fd06beb110822d439a6f
Joseph Pulk <pulkjr@gmail.com>
2015-05-14 20:48:40 -0400

    Initial Construction

---
+++ Performance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetApp;
+using NetApp.Filer;
+
+namespace NTAPPerf
+{
+    class Performance
+    {
+    }
+}
